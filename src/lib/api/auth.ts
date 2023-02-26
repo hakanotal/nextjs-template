7e492848b8faf7a7f3b6c352da31187633bd51e0
ituitis-otal18 <hakan.otal@outlook.com>
2023-02-26 20:32:57 +0300

    v2.1

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,6 +1,7 @@
 import { SignInRequest } from "../schema/SignInRequest";
 import { SignUpRequest } from "../schema/SignUpRequest";
 import { env } from "../../env/client.mjs";
+import axios from "axios";
 
 const API_URL = env.NEXT_PUBLIC_API_URL;
 
@@ -12,13 +13,9 @@ export async function login(email: string, password: string) {
   });
   if (!data.success) throw data.error;
 
-  const response = await fetch(API_URL + "/auth/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data.data),
-  }).then((res) => res.json());
+  const response = await axios
+    .post(API_URL + "/auth/login", data.data)
+    .then((res) => res.data);
 
   if (response.error) throw response.error;
 
@@ -32,13 +29,9 @@ export async function signup(email: string, password: string) {
   });
   if (!data.success) throw data.error;
 
-  const response = await fetch(API_URL + "/auth/signup", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data.data),
-  }).then((res) => res.json());
+  const response = await axios
+    .post(API_URL + "/auth/signup", data.data)
+    .then((res) => res.data);
 
   if (response.error) throw response.error;
 
